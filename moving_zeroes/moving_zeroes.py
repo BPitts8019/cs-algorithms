928031014ddfe17a4ed6fde9b65c2a47f31cb517
BPitts8019 <bradleyj.pitts@gmail.com>
2020-07-14 17:31:01 -0700

    Complete first-pass moving_zeroes

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,20 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
 
-    pass
+
+def moving_zeroes(arr):
+    rtn_arr = arr
+    temp_num = None
+    move_idx = 0
+    for (index, number) in enumerate(arr):
+        if number != 0:
+            temp_num = number
+            rtn_arr[index] = rtn_arr[move_idx]
+            rtn_arr[move_idx] = temp_num
+            move_idx += 1
+
+    return rtn_arr
 
 
 if __name__ == '__main__':
