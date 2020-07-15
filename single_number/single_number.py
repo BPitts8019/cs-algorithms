8efb06f0c8b87b0528a1c744514b486a0a113956
BPitts8019 <bradleyj.pitts@gmail.com>
2020-07-14 17:11:29 -0700

    Add Complete first-pass single_number

--- single_number/single_number.py
+++ single_number/single_number.py
@@ -2,14 +2,25 @@
 Input: a List of integers where every int except one shows up twice
 Returns: an integer
 '''
-def single_number(arr):
-    # Your code here
 
-    pass
+
+def single_number(arr):
+    data = {}
+    for number in arr:
+        if number in data:
+            data[number] += 1
+        else:
+            data[number] = 1
+
+    for (key, value) in data.items():
+        if value < 2:
+            return key
+
+    return None
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
-    arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
+    arr = [1, 3, 4, 4, 5, 5, 1, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
