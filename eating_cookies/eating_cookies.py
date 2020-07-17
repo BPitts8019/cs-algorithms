48c02777a2b366f723b28982e404cffe4cfb3d60
BPitts8019 <bradleyj.pitts@gmail.com>
2020-07-16 18:11:21 -0700

    Optimize eating cookies

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -4,20 +4,25 @@ Returns: an integer
 '''
 
 
-def eating_cookies(n):
+def eating_cookies(n, cache):
+    if n < 0:
+        return 0
     if n <= 1:
+        cache[n] = 1
         return 1
-    if n == 2:
-        return 2
-    if n == 3:
-        return 4
 
-    return eating_cookies(n-1) + eating_cookies(n-2) + eating_cookies(n-3)
+    if cache[n] > 0:
+        number_of_ways = cache[n]
+    else:
+        number_of_ways = eating_cookies(
+            n-1, cache) + eating_cookies(n-2, cache) + eating_cookies(n-3, cache)
+        cache[n] = number_of_ways
+    return number_of_ways
 
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
-    num_cookies = 10
+    num_cookies = 5
 
     print(
-        f"There are {eating_cookies(num_cookies)} ways for Cookie Monster to eat {num_cookies} cookies")
+        f"There are {eating_cookies(num_cookies, [0 for i in range(num_cookies+1)])} ways for Cookie Monster to eat {num_cookies} cookies")
